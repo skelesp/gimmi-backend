94ff18f17da685423f2a562064390fc78c94d1b3
Stijn Beeckmans <stijn.beeckmans@gmail.com>
2016-11-22 16:03:29 +0100

    debugging mongolab_uri usage

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 var url = process.env.MONGOLAB_URI || 'mongodb://localhost/gimmi'
-
+console.log('mongolab_uri:' + process.env.MONGOLAB_URI)
 mongoose.connect(url, function (err) {
   if (err) throw (err);
   console.log('mongodb connected to gimmi database');
