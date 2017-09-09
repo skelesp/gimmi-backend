41e5f0e4c530f4ea02762dd5165cebe8ce6a3d12
Stijn Beeckmans <stijn.beeckmans@gmail.com>
2017-09-09 14:55:26 +0200

    Added documentation in GSCE call

--- server.js
+++ server.js
@@ -47,6 +47,8 @@ app.all("/api/*", function (req, res, next) {
 
 // Google Custom Search route
 app.get('/api/gcse/:search/', function(req, res) {
+  /* GCSE REST API reference : https://developers.google.com/custom-search/json-api/v1/reference/cse/list */
+  /* PRICING (for > 100 request / day) https://developers.google.com/custom-search/json-api/v1/overview#pricing */
   var searchType = "image";
   var url = "https://www.googleapis.com/customsearch/v1?key=" + process.env.GOOGLE_API_KEY
         + "&cx=" + process.env.GOOGLE_CSE_ID
