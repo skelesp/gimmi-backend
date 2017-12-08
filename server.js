06f1e621ef85075d9157a453e9ccc4a8d05e6bf5
Stijn Beeckmans <stijn.beeckmans@gmail.com>
2017-12-08 21:08:58 +0100

    GET wishlist: receiver object added

--- server.js
+++ server.js
@@ -173,10 +173,19 @@ app.get('/api', function (req, res) {
           as: "creator"
         }
       },
-      {$unwind: "$creator"},
+      {
+        $lookup: { //Opgelet:virtuals worden hier niet meegenomen, dus als 'fullname' van een person nodig zou zijn hier, moet dit toegevoegd worden in één van de volgende stages
+          from: "people",
+          localField: "receiver",
+          foreignField: "_id",
+          as: "receiver"
+        }
+      },
+      { $unwind: "$creator" },
+      { $unwind: "$receiver" },
       {
         $group: {
-          _id: { receiverID: "$receiver" },
+          _id: { receiver: "$receiver" },
           wishes: {
             $push: {
               _id: "$_id",
@@ -193,7 +202,8 @@ app.get('/api', function (req, res) {
       },
       {
         $project: {
-          "wishes.createdBy": { "email": 0, "accounts": 0 }
+          "wishes.createdBy": { "email": 0, "accounts": 0 },
+          "_id.receiver": { "email": 0, "accounts": 0 },
         }
       }
     ]).exec( function(err, wishlist){
