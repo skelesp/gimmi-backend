b98f1c26badeb0fc1889ee581fec2a6397c3b525
Stijn Beeckmans <stijn.beeckmans@gmail.com>
2018-01-07 23:16:38 +0100

    Generate all reporting data for lean startup report

--- api/reporting/class.js
+++ api/reporting/class.js
@@ -1,77 +1,93 @@
 var router = require('express').Router();
 var Person = require('../people/model');
 var config = require('../../config');
+var async = require('async');
+var _ = require('lodash');
 
 exports.getLeanstartupData = function(req, res, next) {
-    Person.aggregate([
-        {
-            $project:
-                {
-                    _id: "$_id",
-                    name: { $concat: ["$firstName", " ", "$lastName"] },
-                    registrationMonth: {
-                        $concat: [
-                            { $substr: ["$createdAt", 0, 4] },
-                            "/",
-                            { $substr: ["$createdAt", 5, 2] }
-                        ]
-                    }
-                }
+    async.parallel([
+        function(callback) {
+            getRegistrationData(function(err, reportData){
+                if (err) { return callback(err);}
+                callback(null, reportData);
+            });
         },
-        {
-            $project:
-                {
-                    _id: "$_id",
-                    registrationMonth: "$registrationMonth"
-                }
-        },
-        {
-            $group:
-                {
-                    _id: "$registrationMonth",
-                    registered: { $sum: 1 }
-                }
-        },
-        {
-            $sort:
-                {
-                    "_id": 1
-                }
-        },
-        {
-            $project:
-                {
-                    _id: 0,
-                    month: "$_id",
-                    registered: "$registered"
-                }
-        },
-        {
-            $match:
-                {
-                    month: { $ne: "/" }
-                }
+        function (callback) {
+            getActivationData(function (err, reportData) {
+                if (err) { return callback(err); }
+                callback(null, reportData);
+            });
         }
-    ]).exec(function(err, data) {
-        if (err) { return next(err); }
-        var months = [];
-        var registered = [];
-        
-        data.reduce((sum, value, i) => {
-            return registered[i] = sum + value.registered;    
-        }, 0);
-        data.forEach(function(record){
-            months.push(record.month);
+    ], function(err, results) {
+        // Results = array of results from parallel calls
+        var labels = generatePeriodLabels(new Date(2016,11,1));
+        var data = generateLeanStartupData(labels, results);        
+        res.status(200).json({
+            data: data,
+            labels: labels,
+            series: ['Registered', 'Activated']
         });
-
-        var reportData = {
-            months: months,
-            registered: registered
-        }
-        res.status(200).json(reportData);
     });
 };
-exports.getActivationData = function (req, res, next) {
+
+function generateLeanStartupData (labels, results){
+    var registrationResults = [];
+    var activationResults = [];
+    var registrationCount = 0;
+    var activationCount = 0;
+
+    labels.forEach(function(value, index){
+        var registrationIndex = results[0].months.indexOf(value);
+        var activationIndex = results[1].months.indexOf(value);
+
+        if ( registrationIndex !== -1 ) {
+            // Registration result found
+            registrationCount += results[0].registered[registrationIndex];
+        } 
+        registrationResults[index] = registrationCount;
+
+        if ( activationIndex !== -1 ) {
+            // Activation result found
+            activationCount += results[1].activated[activationIndex];
+        }
+        activationResults[index] = activationCount;
+    });
+
+    return [registrationResults, activationResults] ;
+}
+
+function generatePeriodLabels(startDate, numberOfPeriods){
+    // Check number of periods
+    if (numberOfPeriods > 0) { // Running period with fixed number of periods
+        startDate.setMonth(startDate.getMonth() - numberOfPeriods);
+    } else { // Period from startDate to now (dynamically count periods)
+        numberOfPeriods = monthDiff(startDate, new Date());
+    }
+
+    // Initialize variables
+    var year = startDate.getFullYear();
+    var month = startDate.getMonth() + 1;
+    var reportLabels = [];
+
+    // Loop through all periods and add labels to array
+    for (i = 0; i < numberOfPeriods; i++) {
+        // Push label to array
+        reportLabels.push(year + "/" + _.padStart(month.toString(), 2, "0"));
+        
+        // Change year if 12th month is reached
+        if (month === 12) { 
+            year++;
+            month = 1;
+        } else {
+            month++;
+        }
+    }
+
+    return reportLabels;
+};
+
+function getActivationData (callback){
+    // Get activation data
     Person.aggregate([
         {
             $lookup:
@@ -143,18 +159,97 @@ exports.getActivationData = function (req, res, next) {
                 }
         }
     ]).exec(function (err, data) {
-        if (err) { return next(err); }
+        if (err) { callback(err); }
         
         var months = [];
         var activated = [];
+        
         data.forEach(record => {
             months.push(record.month);
             activated.push(record.activated);
         });
-        var report = {
+        var reportData = {
             months: months,
             activated: activated
         }
-        res.status(200).json(report);
+        callback(null, reportData);
     });
 };
+
+function getRegistrationData (callback){
+    
+    // Get registration data
+    Person.aggregate([
+        {
+            $project:
+                {
+                    _id: "$_id",
+                    name: { $concat: ["$firstName", " ", "$lastName"] },
+                    registrationMonth: {
+                        $concat: [
+                            { $substr: ["$createdAt", 0, 4] },
+                            "/",
+                            { $substr: ["$createdAt", 5, 2] }
+                        ]
+                    }
+                }
+        },
+        {
+            $project:
+                {
+                    _id: "$_id",
+                    registrationMonth: "$registrationMonth"
+                }
+        },
+        {
+            $group:
+                {
+                    _id: "$registrationMonth",
+                    registered: { $sum: 1 }
+                }
+        },
+        {
+            $sort:
+                {
+                    "_id": 1
+                }
+        },
+        {
+            $project:
+                {
+                    _id: 0,
+                    month: "$_id",
+                    registered: "$registered"
+                }
+        },
+        {
+            $match:
+                {
+                    month: { $ne: "/" }
+                }
+        }
+    ]).exec(function (err, data) {
+        if (err) { callback(err); }
+        var months = [];
+        var registered = [];
+        
+        data.forEach(record => {
+            months.push(record.month);
+            registered.push(record.registered)
+        });
+
+        var reportData = {
+            months: months,
+            registered: registered
+        }
+        callback(null, reportData);
+    });
+};
+
+function monthDiff(d1, d2) {
+    var months;
+    months = (d2.getFullYear() - d1.getFullYear()) * 12;
+    months -= d1.getMonth() + 1;
+    months += d2.getMonth();
+    return months <= 0 ? 0 : months;
+}
