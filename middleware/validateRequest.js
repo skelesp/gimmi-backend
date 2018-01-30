f7ecfa0b5a15fb6e6481b26ec81a806860d8b4b1
Stijn Beeckmans <stijn.beeckmans@gmail.com>
2018-01-30 22:07:00 +0100

    add function to support parameters in insecure route

--- middleware/validateRequest.js
+++ middleware/validateRequest.js
@@ -1,30 +1,42 @@
 var jwt = require('jsonwebtoken');
 var config = require('../config');
 
+// List all routes which don't need protection
+var unprotectedRestCalls = [
+    { //authenticate a user
+        route: '/api/authenticate',
+        method: 'POST'
+    },
+    { // Register new person
+        route: '/api/people',
+        method: 'POST'
+    },
+    { // Get list of all people
+        route: '/api/people',
+        method: 'GET'
+    },
+    { // Check if email exists
+        route: '/api/people/email/*',
+        method: 'GET'
+    }
+];
+
 module.exports = function(req, res, next) {
-    // List all routes which don't need protection
-    var unprotectedRestCalls = [
-        { //authenticate a user
-            route: '/api/authenticate',
-            method: 'POST'
-        },
-        { // Register new person
-            route: '/api/people',
-            method: 'POST'
-        },
-        { // Get list of all people
-            route: '/api/people',
-            method: 'GET'
-        }
-    ];
+    
     var protected = true;
 
-    // 
+    // Loop through unprotected calls to check if callRoute corresponds
     for (var i = 0, l = unprotectedRestCalls.length; i < l; i++) {
-        var route = unprotectedRestCalls[i].route;
-        var method = unprotectedRestCalls[i].method;
+        var controlRoute = unprotectedRestCalls[i].route;
+        var controlMethod = unprotectedRestCalls[i].method;
+        var callRoute = req.originalUrl;
+        var callMethod = req.method;
+
+        // replace parameters in callroute for security check
+        callRoute = replaceParamsInCallRoute(callRoute, controlRoute);
+
         // If route + method is an unprotected route --> set protected to false
-        if (req.method === method && req.originalUrl === route) {
+        if (callMethod === controlMethod && callRoute === controlRoute) {
             protected = false;
         }
     }
@@ -56,5 +68,27 @@ module.exports = function(req, res, next) {
         }
     } else { // Route is unprotected, go to next middleware
         next();
+    }
+}
+
+function replaceParamsInCallRoute(callRoute, controlRoute) {
+    controlRouteArr = controlRoute.split('/');
+    changedRouteArr = callRoute.split('/');
+    var changedRoute = "";
+
+    if (changedRouteArr.length === controlRouteArr.length) {
+        controlRouteArr.forEach((controlRoutePart, i) => {
+            if (controlRoutePart === "*") {
+                changedRouteArr[i] = "*";
+            }
+        });
+        changedRoute = changedRouteArr.join('/');
+        controlRoute = controlRouteArr.join('/');
+    }
+
+    if (changedRoute === controlRoute) {
+        return changedRoute;
+    } else {
+        return callRoute;
     }
 }
