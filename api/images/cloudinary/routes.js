5d2d736e67e5f4f2090a2fd21a2a074cdcfdf50b
Stijn Beeckmans <stijn.beeckmans@gmail.com>
2019-02-02 17:42:29 +0100

    Add POST route to image API to upload a new image

--- api/images/cloudinary/routes.js
+++ api/images/cloudinary/routes.js
@@ -3,13 +3,12 @@ var cloudinaryAPI = require('./controller');
 
 imageRouter.route('/signature')
     .post(cloudinaryAPI.generateSignature);
-
 imageRouter.route('/migrateOldImages')
     .put(cloudinaryAPI.migrate);
-
+imageRouter.route('/')
+    .post(cloudinaryAPI.uploadImage);
 imageRouter.route('/:public_id')
     .delete(cloudinaryAPI.deleteImage);
-
 imageRouter.route('/:public_id/public_id')
     .put(cloudinaryAPI.renameImage);
 
