e002e2ac62a3b12485688dcf6adafa6a45c065b8
Stijn Beeckmans <stijn.beeckmans@gmail.com>
2020-10-09 15:34:08 +0200

    changed MONGODB_URI to DB_URI to use with Atlas

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-var url = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/gimmi'
+var url = process.env.DB_URI || 'mongodb://127.0.0.1:27017/gimmi'
 
 mongoose.Promise = global.Promise;
 mongoose.connect(url, {useMongoClient: true}, function (err) {
